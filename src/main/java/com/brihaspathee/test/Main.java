3af83a7522a7e2ada993c75f1d992b2382e23ccc
vbalaji215-dev <vbalaji215@outlook.com>
2025-01-14 19:18:02 -0500

    Initial Add

---
+++ src/main/java/com/brihaspathee/test/Main.java
@@ -0,0 +1,16 @@
+package com.brihaspathee.test;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 14, January 2025
+ * Time: 5:08 PM
+ * Project: Default (Template) Project
+ * Package Name: com.brihaspathee.test
+ * To change this template use File | Settings | File and Code Template
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
